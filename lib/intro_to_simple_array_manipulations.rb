858b03e00e7ca0f39ff77bef0366eac0593b7f65
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-09 03:41:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,7 +46,7 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, Integer)
-  array.delete_at(Integer)
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
 
