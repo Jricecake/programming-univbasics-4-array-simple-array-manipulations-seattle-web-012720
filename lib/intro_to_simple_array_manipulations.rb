7aec6fc9065b1d50c9125603066051b883599112
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-09 03:12:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,30 @@ end
 def using_concat(array)
   
 end
+
+### `using_insert`
+
+This method takes in two arguments, an `Array` and a new element to be added to
+the array. it uses the `.insert` method to add the new element to the 4th index
+of the array.
+
+### `using_uniq`
+
+This method takes in an argument of an `Array` and uses the `.uniq` method to
+remove any duplicate items.
+
+### `using_flatten`
+
+This method takes in an argument of an `Array` that contains other arrays and
+uses the `.flatten` method to return an array of strings.
+
+### `using_delete`
+
+This method takes in two arguments, an `Array` and a `String`, and uses the
+`.delete` method to remove any items from the array that are equal to that
+string.
+
+### `using_delete_at`
+
+This method takes in two arguments, an `Array` and an `Integer` and deletes the
+element at the index of the array that is equal to that integer.
